a6e73ed60c5e9791b1687ab2dbbebc576288b8c0
yvesgurcan <gurcan.yves+github@gmail.com>
2020-09-02 12:00:48 -0700

    Allow S3 origins

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,22 @@ const router = express.Router();
 router.all(
     '*',
     cors({
-        origin: 'http://localhost:3000',
+        origin: function (origin, callback) {
+            // allow requests with no origin (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+            if (
+                [
+                    'http://localhost:3000',
+                    'http://bbt-client-bucket.s3-website-us-west-2.amazonaws.com',
+                    'http://bbt-remote-bucket.s3-website-us-west-2.amazonaws.com'
+                ].indexOf(origin) === -1
+            ) {
+                const msg =
+                    'The CORS policy for this site does not ' +
+                    'allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+            }
+        },
         credentials: true
     })
 );
