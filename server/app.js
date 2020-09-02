03f51ba8a024ea02bb62bece8b3b9361fee0b2d5
yvesgurcan <gurcan.yves+github@gmail.com>
2020-09-02 11:27:31 -0700

    Add methods to headers

--- server/app.js
+++ server/app.js
@@ -13,8 +13,13 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
+        'Origin, X-Requested-With, Content-Type, Accept, authorization'
     );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
     next();
 });
 
